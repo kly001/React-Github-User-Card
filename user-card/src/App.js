d85288a632761a0955f0a35ee7b49e65513a99b9
kly001 <karenly1961@gmail.com>
2019-09-06 01:10:15 -0400

    Users component setup

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import axios from "axios"
+import Users from "./component/Users";
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users:[]
+    }
+  }
+
+    // componentDidMount() {
+    //   fetch("https://api.github.com/users/kly001")
+    //     .then(res => res.json())
+    //     .then(dogs => this.setState({ doggos: dogs.message }))
+    //     .catch(err => console.log("noooo: ", err));
+    // }
+  
+    componentDidMount() {
+      axios
+        .get("https://api.github.com/users/kly001")
+        .then(res => {console.log(res)
+        })
+        .catch(error => console.log(error))
+  }
+    
+
+  render() {
   return (
     <div className="App">
      <h1>React GitHub User Card Project</h1>
+     <Users />
     </div>
   );
+  }
 }
 
 export default App;
---
+++ user-card/src/component/Users.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Users = props => {
+    return(
+        <h2> This is where the users go.</h2>
+    )
+}
+
+export default Users
