30e02b0db94ec80129600daa9b51f0ff302b8175
kly001 <karenly1961@gmail.com>
2019-09-07 13:15:54 -0400

    Connected w/GitHub info; displaying in console

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,31 +1,45 @@
 import React from 'react';
-import axios from "axios"
-import Users from "./component/Users";
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      users:[]
+      user:[],
+      followers:[]
     }
     console.log("State in constructor:",this.state)
   }
   
     componentDidMount() {
-      axios
-        .get("https://api.github.com/users/kly001")
-        .then(res => {console.log("res:",res)
+     fetch("https://api.github.com/users/kly001")
+        .then(res => res.json())
+        .then(res => {this.setState({user: res.data})
+        console.log("User:",res)
         })
         .catch(error => console.log(error))
+       
+
+        fetch("https://api.github.com/users/kly001/followers")
+        .then(res => res.json())
+        .then(res => {this.setState({followers: res.data})
+        console.log("Followers:",res)
+        })
+        .catch(error => console.log(error))
+       
+
+        
   }
   
   render() {
+    console.log("App:",this.state.user)
   return (
     <div className="App">
      <h1>React GitHub User Card Project</h1>
-     <Users />
+     {/* <UserData users = {this.state.users}/> */}
+   
     </div>
+    
   );
   }
 }
